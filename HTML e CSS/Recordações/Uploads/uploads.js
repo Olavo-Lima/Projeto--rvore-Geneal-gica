742569b11f42a9f0322ac2c95705103f5b7965c9
Ollavo Lima <ollavo.augustto@gmail.com>
2023-10-31 21:08:07 -0300

    add mongo

--- HTML e CSS/Recordações/Uploads/uploads.js
+++ HTML e CSS/Recordações/Uploads/uploads.js
@@ -3,7 +3,7 @@ const inserirHtml = document.getElementById('root')
 
 inserirHtml.addEventListener('click', () => {
     const novoHtml = `
-    <form action="/HTML e CSS/Recordações/Uploads/uploads.js" method="post" enctype="multipart/form-data">
+    <form action="/HTML e CSS/Recordações/Uploads/IMG" method="post" enctype="multipart/form-data">
         <input type="file" name="imagem" id="imagem">
         <input type="submit" value="Enviar Imagem">
     </form>
