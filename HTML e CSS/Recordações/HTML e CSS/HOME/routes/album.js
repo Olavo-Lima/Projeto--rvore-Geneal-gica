69687ba15b6c4c969a2d23ca2e7baf519c496d71
Ollavo Lima <ollavo.augustto@gmail.com>
2023-11-24 22:07:16 -0300

    add things

--- HTML e CSS/Recordações/HTML e CSS/HOME/routes/album.js
+++ HTML e CSS/Recordações/HTML e CSS/HOME/routes/album.js
@@ -2,6 +2,7 @@ const natal = document.getElementById('natal')
 const familia = document.getElementById('familia')
 const jem = document.getElementById('jem')
 const junina = document.getElementById('junina')
+const home = document.getElementById('retornar')
 
 natal.addEventListener('click', () => {
     location.href = "./albuns/natal.html"
@@ -19,4 +20,6 @@ junina.addEventListener('click', () => {
     location.href = "./albuns/junina.html"
 })
 
-
+home.addEventListener('click', () => {
+    location.href = '/HTML e CSS/index.html'
+})
